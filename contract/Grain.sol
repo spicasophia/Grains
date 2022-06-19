8f6e37f6fc4c3d1187e37fd206c6eefaaabd582f
FarzeenKist <farzeenkistnareddy@gmail.com>
2022-06-19 16:57:34 +0400

    improved security on smart contract and fixed minor bugs on the frontend

--- contract/Grain.sol
+++ contract/Grain.sol
@@ -7,147 +7,150 @@ import './GrainService.sol';
 
 contract Grain {
 
-  struct Product {
-    address payable owner;
-    string name;
-    string image;
-    string description;
-    string location;
-    uint serviceFee;
-    uint price;
-    uint sold;
-  }
+    struct Product {
+        address payable owner;
+        string name;
+        string image;
+        string description;
+        string location;
+        uint serviceFee;
+        uint price;
+        uint sold;
+    }
 
-  uint internal productsLength = 0;
-  address payable internal onwerAddress;
-  ServiceInterface internal ServiceContract;
-  mapping (uint => Product) internal products;
-  address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+    uint internal productsLength = 0;
+    address payable internal ownerAddress;
+    ServiceInterface internal ServiceContract;
+    mapping (uint => Product) internal products;
+    address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
-  constructor(address serviceContractAddress) {
-    onwerAddress = payable(msg.sender);
-    ServiceContract = ServiceInterface(address(serviceContractAddress));
-  }
+    constructor(address serviceContractAddress) {
+        ownerAddress = payable(msg.sender);
+        ServiceContract = ServiceInterface(address(serviceContractAddress));
+    }
 
-  function writeProduct(
-    string memory _name,
-    string memory _image,
-    string memory _description, 
-    string memory _location,
-    uint _serviceFee,
-    uint _price
-  ) public {
-    uint _sold = 0;
-    products[productsLength] = Product(
-      payable(msg.sender),
-      _name,
-      _image,
-      _description,
-      _location,
-      _serviceFee,
-      _price,
-      _sold
-    );
-    productsLength++;
-  }
 
-  function addService(
-    string memory _name,
-    string memory _image,
-    string memory _description, 
-    string memory _location,
-    string memory _contact,
-    uint _rate
-  ) public {
-    ServiceContract.writeService(_name, _image, _description, _location, _contact, _rate);
-  }
+    modifier checkInputs(
+        string memory _name,
+        string memory _image,
+        string memory _description, 
+        string memory _location
+    ) {
+        require(bytes(_name).length > 0, "Enter a valid name");
+        require(bytes(_image).length > 0, "Enter a valid image url");
+        require(bytes(_description).length > 0, "Enter a valid description");
+        require(bytes(_location).length > 0, "Enter a valid location");
+        _;
+    }
 
-  function readProduct(uint _index) public view returns (
-    address payable owner,
-    string memory name, 
-    string memory image, 
-    string memory description, 
-    string memory location, 
-    uint serviceFee,
-    uint price, 
-    uint sold
-  ) {
-    Product storage product = products[_index];
-    return(
-      product.owner,
-      product.name,
-      product.image,
-      product.description,
-      product.location,
-      product.serviceFee,
-      product.price,
-      product.sold
-    );
-  }
+    function writeProduct(
+        string memory _name,
+        string memory _image,
+        string memory _description, 
+        string memory _location,
+        uint _serviceFee,
+        uint _price
+    ) public checkInputs(_name, _image, _description, _location) {
+        require(_serviceFee > 0, "Enter a valid service fee");
+        require(_price > 0, "Enter a valid price");
+        uint _sold = 0;
+        products[productsLength] = Product(
+        payable(msg.sender),
+        _name,
+        _image,
+        _description,
+        _location,
+        _serviceFee,
+        _price,
+        _sold
+        );
+        productsLength++;
+    }
 
-  function getService(uint _index) public view returns(
-    address user,
-    string memory name, 
-    string memory image, 
-    string memory description, 
-    string memory location, 
-    string memory contact,
-    uint rate,
-    uint hiresLength
-  ) {
-    return ServiceContract.readService(_index);
-  }
+    function addService(
+        string memory _name,
+        string memory _image,
+        string memory _description, 
+        string memory _location,
+        string memory _contact,
+        uint _rate
+    ) public checkInputs(_name, _image, _description, _location) {
+        require(bytes(_contact).length > 9, "Enter a valid contact info"); // ensures that either a valid email or phone number format is entered
+        require(_rate > 0, "Enter a valid rate");
+        ServiceContract.writeService(_name, _image, _description, _location, _contact, _rate);
+    }
 
-  function getServiceHire(uint _serviceIndex, uint _hireIndex) public view returns(
-    address hirer,
-    uint timestamp
-  ) {
-    return ServiceContract.readServiceHire(_serviceIndex, _hireIndex);
-  }
+    function readProduct(uint _index) public view returns (Product memory) {
+        Product storage product = products[_index];
+        return product;
+    }
+
+    function getService(uint _index) public view returns(
+        address user,
+        string memory name, 
+        string memory image, 
+        string memory description, 
+        string memory location, 
+        string memory contact,
+        uint rate,
+        uint hiresLength
+    ) {
+        return ServiceContract.readService(_index);
+    }
+
+    function getServiceHire(uint _serviceIndex, uint _hireIndex) public view returns(
+        address hirer,
+        uint timestamp
+    ) {
+        return ServiceContract.readServiceHire(_serviceIndex, _hireIndex);
+    }
+        
+    // hire a service
+    function hireService(
+        uint _index
+    ) public payable {
+        (address user,,,,,, uint rate,) = ServiceContract.readService(_index); // the remaining returned values are not assigned
+        if(rate > 0) {
+            require(
+            IERC20Token(cUsdTokenAddress).transferFrom(
+                msg.sender,
+                user,
+                rate
+            ),
+            "Failed to hire this service."
+            );
+        }
+
+        ServiceContract.hireService(_index);
+    }
     
-  // hire a service
-  function hireService(
-   uint _index,
-   uint _price,
-   address _serviceUser
-  ) public {
-    require(
-      IERC20Token(cUsdTokenAddress).transferFrom(
-        msg.sender,
-        payable(_serviceUser),
-        _price
-      ),
-      "Failed to hire this service."
-    );
+    function buyProduct(uint _index) public payable  {
+        require(_index < productsLength, "Enter a valid index");
+        require(msg.sender != products[_index].owner, "You can't buy your own product");
+        require(
+        IERC20Token(cUsdTokenAddress).transferFrom(
+            msg.sender,
+            ownerAddress,
+            products[_index].serviceFee
+        ),
+        "Product fee transfer failed."
+        );
+        require(
+        IERC20Token(cUsdTokenAddress).transferFrom(
+            msg.sender,
+            products[_index].owner,
+            products[_index].price
+        ),
+        "Product price transfer failed."
+        );
+        products[_index].sold++;
+    }
+    
+    function getProductsLength() public view returns (uint) {
+        return (productsLength);
+    }
 
-    ServiceContract.hireService(_index);
-  }
-  
-  function buyProduct(uint _index) public payable  {
-    require(
-      IERC20Token(cUsdTokenAddress).transferFrom(
-        msg.sender,
-        onwerAddress,
-        products[_index].serviceFee
-      ),
-      "Product fee transfer failed."
-    );
-    require(
-      IERC20Token(cUsdTokenAddress).transferFrom(
-        msg.sender,
-        products[_index].owner,
-        products[_index].price
-      ),
-      "Product price transfer failed."
-    );
-    products[_index].sold++;
-  }
-  
-  function getProductsLength() public view returns (uint) {
-    return (productsLength);
-  }
-
-  function getServicesLength() public view returns (uint) {
-    return ServiceContract.readServicesLength();
-  }
+    function getServicesLength() public view returns (uint) {
+        return ServiceContract.readServicesLength();
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -7,34 +7,34 @@ module.exports = {
   mode: "development",
   devtool: "cheap-module-eval-source-map",
   entry: {
-    main: path.resolve(process.cwd(), "src", "main.js")
+    main: path.resolve(process.cwd(), "src", "main.js"),
   },
   output: {
     path: path.resolve(process.cwd(), "docs"),
-    publicPath: ""
+    publicPath: "",
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
-	node: {
-   fs: "empty",
-	 net: "empty"
-	},
+  node: {
+    fs: "empty",
+    net: "empty",
+  },
   watchOptions: {
     // ignored: /node_modules/,
     aggregateTimeout: 300, // After seeing an edit, wait .3 seconds to recompile
-    poll: 500 // Check for edits every 5 seconds
+    poll: 500, // Check for edits every 5 seconds
   },
   plugins: [
     new FriendlyErrorsWebpackPlugin(),
     new webpack.ProgressPlugin(),
     new HtmlWebpackPlugin({
-      template: path.resolve(process.cwd(), "public", "index.html")
-    })
-  ]
-}
+      template: path.resolve(process.cwd(), "public", "index.html"),
+    }),
+  ],
+};
